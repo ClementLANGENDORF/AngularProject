ea580c00ad84a9d0fb11a340a68af635a938c646
KoXo <clement.langendorf@gmail.com>
2019-01-23 12:59:10 +0100

    Modif header

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logIn: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.logIn = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogInSubmit() {
+    this.submitted = true;
+
+    if (this.logIn.invalid) {
+      return;
+    }
+
+    this.success = true;
   }
 
 }
