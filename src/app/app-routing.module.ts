764c9b61a24d0a1cdaf5b607490c4ad0ef907e7b
KoXo <clement.langendorf@gmail.com>
2019-01-24 09:30:25 +0100

    Routing subscribe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent} from './login/login.component';
+import { LoginComponent } from './login/login.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  {path: 'subscribe', component: UserFormComponent},
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
